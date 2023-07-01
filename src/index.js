be4ea6c47583ba066c9207e4bfc96b9e956f26e8
Hsiao Yao <tomtogo1260@gmail.com>
2023-07-01 12:54:20 +0800

    volume viewer init setup

--- src/index.js
+++ src/index.js
@@ -1,55 +1,54 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
-// Sizes
-const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
-}
+let scene, camera, cube, controls, renderer
 
-window.addEventListener('resize', () =>
-{
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
+init()
 
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
+async function init() {
+    // Sizes
+    const sizes = {
+        width: window.innerWidth,
+        height: window.innerHeight
+    }
 
+    window.addEventListener('resize', () =>
+    {
+        sizes.width = window.innerWidth
+        sizes.height = window.innerHeight
+
+        camera.aspect = sizes.width / sizes.height
+        camera.updateProjectionMatrix()
+
+        renderer.setSize(sizes.width, sizes.height)
+    })
+
+    // Scene
+    scene = new THREE.Scene()
+
+    // Camera
+    camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+    camera.position.z = 3
+    scene.add(camera)
+
+    // Test
+    cube = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), new THREE.MeshNormalMaterial())
+    scene.add(cube)
+
+    renderer = new THREE.WebGLRenderer({ antialias: true })
+    renderer.setPixelRatio(window.devicePixelRatio)
     renderer.setSize(sizes.width, sizes.height)
-})
+    document.body.appendChild(renderer.domElement)
 
-// Scene
-const scene = new THREE.Scene()
+    // Controls
+    controls = new OrbitControls(camera, renderer.domElement)
+    controls.addEventListener('change', render)
 
-// Camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.z = 3
-scene.add(camera)
-
-// Test
-const cube = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), new THREE.MeshNormalMaterial())
-scene.add(cube)
-
-const renderer = new THREE.WebGLRenderer({ antialias: true })
-renderer.setPixelRatio(window.devicePixelRatio)
-renderer.setSize(sizes.width, sizes.height)
-
-// Renderer
-const canvas = renderer.domElement
-document.body.appendChild(canvas)
-
-// Controls
-const controls = new OrbitControls(camera, canvas)
-controls.target = new THREE.Vector3(0,0,0)
-controls.enableDamping = true
-
-const tick = () =>
-{
-    cube.rotation.y += 0.01
-
-    controls.update()
-    renderer.render(scene, camera)
-
-    window.requestAnimationFrame(tick)
+    render()
 }
-tick()
+
+function render() {
+    if (!renderer) return
+
+    renderer.render(scene, camera)
+}
